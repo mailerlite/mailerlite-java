5f05bc824d4638bc2d787b4aa1a71f5bda790bcc
johnkelesidis <john@mailerlite.com>
2023-12-06 13:27:44 +0200

    automation triggers deserializer

--- src/main/java/com/mailerlite/sdk/automations/AutomationDeserializer.java
+++ src/main/java/com/mailerlite/sdk/automations/AutomationDeserializer.java
@@ -29,6 +29,16 @@ import com.mailerlite.sdk.automations.steps.condition.CustomFieldsCondition;
 import com.mailerlite.sdk.automations.steps.condition.GroupMembershipCondition;
 import com.mailerlite.sdk.automations.steps.condition.SegmentMembershipCondition;
 import com.mailerlite.sdk.automations.steps.condition.WorkflowActivityCondition;
+import com.mailerlite.sdk.automations.triggers.AutomationTrigger;
+import com.mailerlite.sdk.automations.triggers.TriggerAbandonedCart;
+import com.mailerlite.sdk.automations.triggers.TriggerClicksLink;
+import com.mailerlite.sdk.automations.triggers.TriggerDate;
+import com.mailerlite.sdk.automations.triggers.TriggerFieldUpdated;
+import com.mailerlite.sdk.automations.triggers.TriggerFormCompleted;
+import com.mailerlite.sdk.automations.triggers.TriggerPurchasedAnyProduct;
+import com.mailerlite.sdk.automations.triggers.TriggerPurchasedFromCategory;
+import com.mailerlite.sdk.automations.triggers.TriggerPurchasedSpecificProduct;
+import com.mailerlite.sdk.automations.triggers.TriggerSubscriberJoinsGroup;
 import com.mailerlite.sdk.util.JsonSerializationDeserializationStrategy;
 
 public class AutomationDeserializer implements JsonDeserializer<Automation> {
@@ -80,6 +90,63 @@ public class AutomationDeserializer implements JsonDeserializer<Automation> {
 		}
 		
 		automation.steps = steps.toArray();
+		
+		
+		JsonArray automationTriggers = jsonObject.get("triggers").getAsJsonArray();
+		
+		ArrayList<AutomationTrigger> triggers = new ArrayList<AutomationTrigger>();
+		
+		for (JsonElement jsonTrigger : automationTriggers) {
+			
+			JsonObject triggerObj = jsonTrigger.getAsJsonObject();
+			
+			String type = triggerObj.get("type").getAsString();
+			
+			switch (type) {
+			
+				case "subscriber_joins_group":
+					triggers.add(gson.fromJson(jsonObject, TriggerSubscriberJoinsGroup.class));
+					break;
+					
+				case "clicks_link":
+					triggers.add(gson.fromJson(jsonObject, TriggerClicksLink.class));
+					break;
+					
+				case "field_updated":
+					triggers.add(gson.fromJson(jsonObject, TriggerFieldUpdated.class));
+					break;
+					
+				case "anniversary":
+					triggers.add(gson.fromJson(jsonObject, TriggerDate.class));
+					break;
+					
+				case "exact_date":
+					triggers.add(gson.fromJson(jsonObject, TriggerDate.class));
+					break;
+					
+				case "form_completed":
+					triggers.add(gson.fromJson(jsonObject, TriggerFormCompleted.class));
+					break;
+					
+				case "purchased_any_product":
+					triggers.add(gson.fromJson(jsonObject, TriggerPurchasedAnyProduct.class));
+					break;
+					
+				case "purchased_specific_product":
+					triggers.add(gson.fromJson(jsonObject, TriggerPurchasedSpecificProduct.class));
+					break;
+					
+				case "purchased_from_category":
+					triggers.add(gson.fromJson(jsonObject, TriggerPurchasedFromCategory.class));
+					break;
+					
+				case "abandoned_cart":
+					triggers.add(gson.fromJson(jsonObject, TriggerAbandonedCart.class));
+					break;
+			}
+		}
+		
+		automation.triggers = triggers.toArray();
 		
 		return automation;
 	}
